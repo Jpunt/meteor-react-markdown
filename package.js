88645b90b66f138fda43e631f0da9e59cabcb327
Jasper Haggenburg <jasperh@q42.nl>
2015-11-06 12:30:46 +0100

    removed meteor from the name

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
-  name: 'q42:meteor-react-markdown',
+  name: 'q42:react-markdown',
   version: '0.0.1',
   summary: 'React component to parse markdown with Marked',
-  git: '',
+  git: 'https://github.com/Jpunt/meteor-react-markdown.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use('ecmascript');
-  api.addFiles('meteor-react-markdown.js');
+  api.addFiles('react-markdown.js');
 });
