77b1c030017819212df83f248a030867262c659d
Jasper Haggenburg <jasperh@q42.nl>
2015-12-07 15:28:53 +0100

    bumped version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'q42:react-markdown',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'React component to parse markdown in Meteor',
   git: 'https://github.com/Jpunt/meteor-react-markdown.git',
   documentation: 'README.md'
