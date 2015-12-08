2d33989451730bcf7596634602f7c52add835e70
Jasper Haggenburg <jasperh@q42.nl>
2015-12-08 12:13:01 +0100

    updated react

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'q42:react-markdown',
-  version: '0.0.2',
+  version: '0.0.3',
   summary: 'React component to parse markdown in Meteor',
   git: 'https://github.com/Jpunt/meteor-react-markdown.git',
   documentation: 'README.md'
@@ -9,7 +9,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.2.0.1');
   api.use('ecmascript');
-  api.use('react@0.1.13');
+  api.use('react@0.14.3');
   api.use('perak:markdown@1.0.5');
   api.addFiles('react-markdown.jsx');
   api.export('Markdown', ['client', 'server']);
